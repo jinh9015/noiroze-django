55ef4f253106366ef73e2f124bd2eff011da9c66
Khancording <digitwk@gmail.com>
2023-06-17 15:20:00 +0900

    메인페이지 base.html 로그아웃 기능 처리

--- NoiroseServer/api/urls.py
+++ NoiroseServer/api/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     path('user_list/', UserListView.as_view()),             # api/user_list/   로 엔드포인트 설정. 유저 목록 확인 가능.
     path('user_register/', UserRegisterView.as_view(), name='user_register'),     # api/user_register/  회원가입 시 데이터 저장되는 엔드포인트
     path('user_login/', UserLoginView.as_view(), name='user_login'),           # api/user_login/     로그인 시도시, 자격증명 검증하는 엔드포인트
-    path('user_logout/', UserLogoutView.as_view(), name='logout'),
+    path('user_logout/', UserLogoutView.as_view(), name='user_logout'),               # api/user_logout/  로그아웃 시, 토큰을 제거하는 엔드포인트
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),          # 사용자 토큰 생성
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),         # 사용자 토큰 리프레시
 
