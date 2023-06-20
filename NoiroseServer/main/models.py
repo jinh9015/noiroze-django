6d8e5bef88ee462231fb736863509f9765aecb35
Jinhyeok Chang <jinyokc@Jinhyeokui-MacBookAir.local>
2023-06-20 11:26:15 +0900

    sound_level_verified nullable 추가

--- NoiroseServer/main/models.py
+++ NoiroseServer/main/models.py
@@ -69,7 +69,7 @@ class Sound_Level_Verified(models.Model):
         ('방2', '방2'),
     )
     place = models.CharField(max_length=10, choices=PLACE_CHOICES, default='거실')       # 센서 설치 장소
-    value = models.FloatField()                   # 센서 값 ( dB(A) )
-    created_at = models.DateTimeField()           # 측정 날짜-시간
-    sound_type = models.CharField(max_length=100)  
-    file_name = models.CharField(max_length=40)   # 녹음된 파일 명
+    value = models.FloatField(null=True)                   # 센서 값 ( dB(A) )
+    created_at = models.DateTimeField(null=True)           # 측정 날짜-시간
+    sound_type = models.CharField(max_length=100, null=True)  
+    file_name = models.CharField(max_length=40, null=True)   # 녹음된 파일 명
