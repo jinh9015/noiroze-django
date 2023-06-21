1ea0f28d034af8b205b4728fe4a8f7f31066d89c
Jinhyeok Chang <jinyokc@Jinhyeokui-MacBookAir.local>
2023-06-21 16:27:52 +0900

    add boto3

--- NoiroseServer/main/views.py
+++ NoiroseServer/main/views.py
@@ -3,6 +3,8 @@ from . import views
 from . import models
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+import boto3
+import os
 
 # Create your views here.
 def base_request(request):
@@ -22,6 +24,13 @@ def download_sound_file(request):
         model = models.Sound_File(dong=dong, ho=ho, file_name=file_name, sound_file=sound_file)
         model.save()
 
+        # 저장된 파일을 AWS S3의 'noiroze-noisefile-backup' 버킷에 업로드
+        s3 = boto3.client('s3')
+        media_directory = "media/"
+        file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), media_directory, file_name)
+        file_key = media_directory + file_name
+        s3.upload_file(file_path, 'noiroze-noisefile-backup', file_key)
+
         print('download sound file', dong, ho, file_name, sound_file)
         msg = {'result': 'success'}
 
@@ -29,4 +38,3 @@ def download_sound_file(request):
         msg = {'result': 'fail'}
 
     return JsonResponse(msg)
-
