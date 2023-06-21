d02fe83b00c92f0d13c86752df069333e51d9aca
Jinhyeok Chang <jinyokc@Jinhyeokui-MacBookAir.local>
2023-06-21 18:20:13 +0900

    edit directory

--- NoiroseServer/main/views.py
+++ NoiroseServer/main/views.py
@@ -12,6 +12,15 @@ aws_secret_key = os.environ.get("AWS_SECRET_KEY")
 # S3 인증 정보 추가
 s3 = boto3.client('s3', aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key)
 
+def upload_directory_to_s3(local_directory, bucket_name, s3_directory, s3_client):
+    for root, dirs, files in os.walk(local_directory):
+        for filename in files:
+            local_path = os.path.join(root, filename)
+            relative_path = os.path.relpath(local_path, local_directory)
+            s3_path = os.path.join(s3_directory, relative_path)
+            s3_client.upload_file(local_path, bucket_name, s3_path)
+            print(f"Uploaded {local_path} to s3://{bucket_name}/{s3_path}")
+
 # Create your views here.
 def base_request(request):
     
@@ -31,14 +40,14 @@ def download_sound_file(request):
         model.save()
 
         # 저장된 파일을 AWS S3의 'noiroze-noisefile-backup' 버킷에 업로드
-        s3 = boto3.client('s3')
         media_directory = "../media/"
-        file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), media_directory, file_name)
-        file_key = media_directory + file_name
-        try:
-            s3.upload_file(file_path, 'noiroze-noisefile-backup', file_key)
-        except Exception as e:
-            print("Error uploading file to S3:", e)
+        local_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), media_directory)
+        bucket_name = 'noiroze-noisefile-backup'
+        s3_directory = 'media/'
+
+        # 모든 media 폴더 내 파일들을 S3 버킷에 업로드
+        upload_directory_to_s3(local_directory, bucket_name, s3_directory, s3)
+
         print('download sound file', dong, ho, file_name, sound_file)
         msg = {'result': 'success'}
 
