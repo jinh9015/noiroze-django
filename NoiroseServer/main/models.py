f99eb22749ecdcb35e604cd7435d55c6dc4721bd
Khancording <digitwk@gmail.com>
2023-06-22 17:09:03 +0900

    커뮤니티 게시판 모델 카테고리 추가

--- NoiroseServer/main/models.py
+++ NoiroseServer/main/models.py
@@ -148,7 +148,7 @@ class Sound_Level_Verified(models.Model):
 
 
 class CommunityBoard(models.Model):                            # 커뮤니티 게시판 모델
-    CATEGORY_CHOICES = ('정보공유', '소통해요', '붙어봐요')
+    CATEGORY_CHOICES = ('정보공유', '소통해요', '붙어봐요', '밥먹어요')
     category = models.CharField(max_length=10, choices=CATEGORY_CHOICES, default='정보공유')       
     title = models.CharField(max_length=200) 
     content = models.TextField()  
