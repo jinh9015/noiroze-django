75c2595942eed9227319a07d75a43e8e939bbc01
kimseokkyu <95takeitslow@gmail.com>
2023-06-30 12:20:52 +0900

    게시판 수정

--- NoiroseServer/main/urls.py
+++ NoiroseServer/main/urls.py
@@ -8,9 +8,18 @@ urlpatterns = [
 
     path('base/', views.base_request, name='base'),
     path('dashboard/', views.dash_request, name='dashboard'),
-    path('question/create/', views.question_create, name='question_create'),  # 수정된 부분
+
+    # 컴플레인 보드
+    path('complain_board_list/', views.complain_board_list, name='complain_board_list'),
+    path('update_complain_board/<int:complain_board_id>/', views.update_complain_board, name='update_complain_board'),
+    path('delete_complain_board/<int:complain_board_id>/', views.delete_complain_board, name='delete_complain_board'),
+    path('create_complain_board/', views.create_complain_board, name='create_complain_board'),
+    path('complain_board_detail/<int:pk>/', views.complain_board_detail, name='complain_board_detail'),
+    path('add_reply/<int:community_board_pk>/', views.add_reply, name='add_reply'),
 
     path('community_board_list/', views.community_board_list, name='community_board_list'),
+    path('create_community_board/', views.create_community_board, name='create_community_board'),
+
     path('sound_file/', views.download_sound_file, name='sound_file'),
 ]
 
--- NoiroseServer/main/views.py
+++ NoiroseServer/main/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect, get_object_or_404
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.db.models import Q, Count
 from django.core.paginator import Paginator
-
 from .models import *
 from .forms import ComplainBoardForm
+from rest_framework.authtoken.models import Token
+from django.contrib import messages
+from main.models import ComplainBoard
 
 # Create your views here.
 def base_request(request):
@@ -13,9 +15,6 @@ def base_request(request):
 
 def dash_request(request):
     return render(request, 'dash.html')
-
-
-
 
 
 # 녹음파일 서버에 저장하는 함수
@@ -74,24 +73,169 @@ def community_board_list(request):
     paginator = Paginator(board_list, 10)  # 페이지당 10개
     page_obj = paginator.get_page(page)
     context = {'board_list': page_obj, 'page': page, 'kw': kw, 'so': so}
-    return render(request, 'board/question_list.html', context)
+    return render(request, 'board/community_board/community_board_list.html', context)
 
-
-
-def question_create(request):
-    '''
-    질문 생성 폼 및 처리
-    '''
+def create_community_board(request):
     if request.method == 'POST':
         form = ComplainBoardForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('main:community_board_list')
+            new_board = form.save(commit=False)
+            # 현재 로그인된 사용자를 author에 저장
+            token_key = request.COOKIES.get('auth-token')   # 쿠키에서 토큰 가져오기
+            try:
+                token = Token.objects.get(key=token_key)    # 토큰으로 사용자 정보 가져오기
+            except Token.DoesNotExist:
+                messages.error(request, '로그인이 필요합니다.')
+                return redirect('/common/login/')
+            new_board.author = token.user
+            new_board.save()
+            return redirect('/main/community_board_list/')
     else:
         form = ComplainBoardForm()
-
-    context = {'form': form}
-    return render(request, 'question_create.html', context)
-
+        
+    return render(request, 'board/community_board/create_community_board.html', {'form': form})
 
 
+######### 컴플레인 보드 관련 함수 시작
+def create_complain_board(request):
+    if request.method == 'POST':
+        form = ComplainBoardForm(request.POST)
+        if form.is_valid():
+            new_board = form.save(commit=False)
+            # 현재 로그인된 사용자를 author에 저장
+            token_key = request.COOKIES.get('auth-token')   # 쿠키에서 토큰 가져오기
+            try:
+                token = Token.objects.get(key=token_key)    # 토큰으로 사용자 정보 가져오기
+            except Token.DoesNotExist:
+                messages.error(request, '로그인이 필요합니다.')
+                return redirect('/common/login/')
+            new_board.author = token.user
+            new_board.save()
+            return redirect('/main/complain_board_list/')
+    else:
+        form = ComplainBoardForm()
+        
+    return render(request, 'board/complain_board/create_complain_board.html', {'form': form})
+
+def update_complain_board(request, complain_board_id):
+    complain_board = get_object_or_404(ComplainBoard, id=complain_board_id)
+
+    if request.method == 'POST':
+        form = ComplainBoardForm(request.POST, instance=complain_board)
+        if form.is_valid():
+            # 현재 로그인된 사용자를 확인
+            token_key = request.COOKIES.get('auth-token')   # 쿠키에서 토큰 가져오기
+
+            try:
+                token = Token.objects.get(key=token_key)    # 토큰으로 사용자 정보 가져오기
+                if token.user == complain_board.author:     # 유효한 사용자 확인
+                    updated_board = form.save(commit=False)
+                    updated_board.author = token.user
+                    updated_board.save()
+                    return redirect('/main/complain_board_list/')
+                else:
+                    messages.error(request, '수정 권한이 없습니다.')
+                    return redirect('/main/complain_board_list/')
+            except Token.DoesNotExist:
+                messages.error(request, '로그인이 필요합니다.')
+                return redirect('/common/login/')
+    else:
+        form = ComplainBoardForm(instance=complain_board)
+
+    return render(request, 'board/complain_board/update_complain_board.html', {'form': form, 'complain_board_id': complain_board_id})
+    
+def delete_complain_board(request, complain_board_id):
+    complain_board = get_object_or_404(ComplainBoard, id=complain_board_id)
+
+    if request.method == 'POST':
+        # 현재 로그인된 사용자를 확인
+        token_key = request.COOKIES.get('auth-token')  # 쿠키에서 토큰 가져오기
+
+        if token_key:
+            try:
+                token = Token.objects.get(key=token_key)  # 토큰으로 사용자 정보 가져오기
+                if token.user == complain_board.author:   # 유효한 사용자 확인
+                    complain_board.delete()
+                    return redirect('main:complain_board_list')
+                else:
+                    messages.error(request, '삭제 권한이 없습니다.')
+                    return redirect('/main/complain_board_list/')
+            except Token.DoesNotExist:
+                messages.error(request, '로그인이 필요합니다.')
+                return redirect('/common/login/')
+        else:
+            messages.error(request, '로그인이 필요합니다.')
+            return redirect('/common/login/')
+
+    context = {
+        'form': ComplainBoardForm(instance=complain_board)
+    }
+    return render(request, 'board/complain_board/delete_complain_board.html', context)
+
+def complain_board_list(request):
+    '''
+    board_list 출력
+    '''
+
+    # 입력 인자
+    page = request.GET.get('page', '1')  # 페이지
+    kw = request.GET.get('kw', '')  # 검색어
+    so = request.GET.get('so', 'recent')  # 정렬 기준 / default 최신순
+
+    if so == 'recent':
+        board_list = ComplainBoard.objects.order_by('-created_date')
+    elif so == 'late':
+        board_list = ComplainBoard.objects.order_by('created_date')
+    elif so == 'recommend':
+        board_list = ComplainBoard.objects.annotate(
+            num_voter=Count('voter')).order_by('-num_voter', '-created_date')
+    elif so == 'popular':
+        board_list = ComplainBoard.objects.annotate(
+            num_reply=Count('reply')).order_by('-num_reply', '-created_date')
+    else:  # 위 경우 제외 board_id 역순정렬
+        board_list = ComplainBoard.objects.order_by('-id')
+
+    if kw:
+        kw = kw.replace('년', '')
+        kw = kw.replace('월', '')
+        kw = kw.replace('일', '')
+        board_list = board_list.filter(
+            Q(title__icontains=kw) |  # 제목 검색
+            Q(content__icontains=kw) |  # 내용 검색
+            Q(author__name__icontains=kw) |  # 작성자 검색
+            Q(club__name__icontains=kw) |  # 클럽 이름 검색
+            Q(club__category__icontains=kw) |  # 클럽 카테고리 검색
+            Q(event_date__icontains=kw)  # 모임일 검색
+        ).distinct()
+
+    paginator = Paginator(board_list, 10)  # 페이지당 10개
+    page_obj = paginator.get_page(page)
+    context = {'board_list': page_obj, 'page': page, 'kw': kw, 'so': so}
+    return render(request, 'board/complain_board/complain_board_list.html', context)
+
+
+
+def complain_board_detail(request, pk):
+    board = get_object_or_404(ComplainBoard, pk=pk)
+    return render(request, 'board/complain_board/complain_board_detail.html', {'board': board})
+
+def add_reply(request, pk):
+    if request.method == "POST":
+        complain_board = get_object_or_404(ComplainBoard, pk=pk)
+        content = request.POST['content']
+
+        # 현재 로그인된 사용자를 author에 저장
+        token_key = request.COOKIES.get('auth-token')  # 쿠키에서 토큰 가져오기
+        try:
+            token = Token.objects.get(key=token_key)  # 토큰으로 사용자 정보 가져오기
+        except Token.DoesNotExist:
+            messages.error(request, '로그인이 필요합니다.')
+            return redirect('/common/login/')
+
+        author = token.user
+
+        Reply.objects.create(community_board=complain_board, author=author, content=content)
+
+        return redirect('main:complain_board_detail', pk)
+
+#   컴플레인 보드 관련 함수 끝  #
